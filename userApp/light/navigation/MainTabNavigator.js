7195a7bff0b608c587c3e5b130c95677a71e6ba5
Scott Mercer <37919395+scottmercer@users.noreply.github.com>
2018-10-22 12:46:17 -0500

    Add files via upload

---
+++ userApp/light/navigation/MainTabNavigator.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import UsersScreen from '../screens/UsersScreen';
+import CameraScreen from '../screens/CameraScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const UsersStack = createStackNavigator({
+  Links: UsersScreen,
+});
+
+UsersStack.navigationOptions = {
+  tabBarLabel: 'Users',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-contacts${focused ? '' : '-outline'}` : 'md-contacts'}
+    />
+  ),
+};
+
+const CameraStack = createStackNavigator({
+  Settings: CameraScreen,
+});
+
+CameraStack.navigationOptions = {
+  tabBarLabel: 'Camera',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-camera${focused ? '' : '-outline'}` : 'md-camera'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  UsersStack,
+  CameraStack,
+});

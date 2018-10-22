46af5aa861efefc6be1a426137850e3f1f15e007
Scott Mercer <randall.mercer@yahoo.com>
2018-10-22 14:00:01 -0500

    files and folders update for expo app

--- App.js
+++ App.js
@@ -1,65 +1,61 @@
 import React from 'react';
-import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
-import { List, ListItem } from "react-native-elements";
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { AppLoading, Asset, Font, Icon } from 'expo';
+import AppNavigator from './navigation/AppNavigator';
 
 export default class App extends React.Component {
   state = {
-    data: [],
-    page: 0,
-    loading: false
-  };
-
-  componentWillMount() {
-    this.fetchData();
-  }
-
-  fetchData = async () => {
-    this.setState({ loading: true });
-    const response = await fetch(
-      `https://randomuser.me/api?results=15&seed=hi&page=${this.state.page}`
-    );
-    const json = await response.json();
-    this.setState(state => ({
-      data: [...state.data, ...json.results],
-      loading: false
-    }));
-  };
-
-  handleEnd = () => {
-    this.setState(state => ({ page: state.page + 1 }), () => this.fetchData());
+    isLoadingComplete: false,
   };
 
   render() {
-    return (
-      <View>
-        <List>
-          <FlatList
-            data={this.state.data}
-            keyExtractor={(x, i) => i}
-            onEndReached={() => this.handleEnd()}
-            onEndReachedThreshold={0}
-            ListFooterComponent={() =>
-              this.state.loading
-                ? null
-                : <ActivityIndicator size="large" animating />}
-            renderItem={({ item }) =>
-              <ListItem
-                roundAvatar
-                avatar={{ uri: item.picture.thumbnail }}
-                title={`${item.name.first} ${item.name.last}`}
-              />}
-          />
-        </List>
-      </View>
-    );
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      );
+    }
   }
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require('./assets/images/lac.png'),
+      ]),
+      Font.loadAsync({
+        // This is the font that we are using for our tab bar
+        ...Icon.Ionicons.font,
+        // We include SpaceMono because we use it in HomeScreen.js. Feel free
+        // to remove this if you are not using it in your app
+        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+      }),
+    ]);
+  };
+
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- README.md
+++
@@ -1,8 +0,0 @@
-# UserApp
-Express/React Native app for generating users as well as taking pictures and saving them to the camera roll. 
-
-To Run:
-1. yarn install
-2. CD light
-3. yarn install
-4. expo start
